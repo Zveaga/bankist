2812db50d964137c9d5f7c15d8b4fb98a708bd92
Rares <rares.rares.cox@gmail.com>
2024-09-27 13:33:40 +0200

    updated accounts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //----------------Data----------------//
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Rar Ang',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
